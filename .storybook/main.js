d910b16e11495663f4d0cb096ad7716311d5d71d
nionoku <nionoku@gmail.com>
2021-09-30 00:10:37 +0300

    feat: Add support vuetify to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,49 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.ts"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    '@socheatsok78/storybook-addon-vuetify'
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, "../src/"),
+      vue: 'vue/dist/vue.js',
+      'vue$': 'vue/dist/vue.esm.js',  
+    };
+
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Use Sass loader for vuetify components
+    config.module.rules.push({
+      test: /\.sass$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            additionalData: "@import '@/styles/variables.scss';"
+          }
+        }
+      ],
+    });
+
+
+    return config;
+  }
 }
